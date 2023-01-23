b15e52aef960fd790852881f97a83f9d8e13e79c
Valentin Kosyakov <valentin.kosyakovEnd@yandex.ru>
2023-01-23 16:01:42 +0300

    Generated top of the Header

--- src/Components/Header/SubComponents/HeaderTop.jsx
+++ src/Components/Header/SubComponents/HeaderTop.jsx
@@ -4,7 +4,22 @@ import styles from './subComponent.module.css'
 const HeaderTop = props => {
     return (
         <div className={styles.content}>
-             
+            <div className={styles.left}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Russia.svg" alt="" className={styles.flag} />
+                <p className={styles.language}>UA|RU</p>
+            </div>
+            <div className={styles.center}>
+                <p className={`${styles.center__links} ${styles.center__point}`}>Россия</p>
+                <p className={styles.center__links}>Магазины</p>
+                <p className={styles.center__links}>Поиск на карте</p>
+                <p className={styles.center__links}>Вход|Регистрация</p>
+            </div>
+            <div className={styles.right}>
+                <img src="images/Header/profile.svg" alt="" className={styles.right__img} />
+                <img src="images/Header/delivery.svg" alt="" className={styles.right__img} />
+                <img src="images/Header/backet.svg" alt="" className={styles.right__img} />
+                <img src="images/Header/favorite.svg" alt="" className={styles.right__img} />
+            </div>
         </div>
     )
 }
