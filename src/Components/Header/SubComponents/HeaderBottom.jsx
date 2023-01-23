5fe767e8ed323eb23a763533d11a75a4c715c03f
Valentin Kosyakov <valentin.kosyakovEnd@yandex.ru>
2023-01-23 16:19:15 +0300

    Generate bottom of the Header

--- src/Components/Header/SubComponents/HeaderBottom.jsx
+++ src/Components/Header/SubComponents/HeaderBottom.jsx
@@ -1,9 +1,21 @@
-import styles from './subComponent.module.css'
+import styles from './subComponent2.module.css'
 
 
 const HeaderBottom = props => {
-    return(
-        <div className={styles.content}></div>
+    return (
+        <div className={styles.content}>
+            <div className={styles.left}>
+                <h1 className={styles.logo}>VamBeri</h1>
+            </div>
+            <div className={styles.center}>
+                <textarea name="Find" className={styles.find} placeholder="Поиск по названиям" />
+                <p className={`${styles.find__button} ${styles.find__button__categories}`}>Категории</p>
+                <p className={`${styles.find__button} ${styles.find__button__find}`}>Поиск</p>
+            </div>
+            <div className={styles.right}>
+                <p className={styles.create__ad}>Создать объявление</p>
+            </div>
+        </div>
     )
 }
 
